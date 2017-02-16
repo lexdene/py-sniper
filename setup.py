e461a5078b75793796a34952657bc147c35587c4
Elephant Liu <lexdene@gmail.com>
2017-02-17 00:01:56 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ setup(
     name="sniper",
     version="0.0.1",
     description="a restful api framework base on asyncio",
+    url="https://github.com/lexdene/py-sniper",
+    author="Elephant Liu",
+    author_email="lexdene@gmail.com",
+    packages=['sniper'],
 )

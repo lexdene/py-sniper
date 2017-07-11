8791ac09dd5cd27d7070002e22e187e9f6808632
Elephant Liu <lexdene@gmail.com>
2017-07-11 20:38:32 +0800

    update cached property

--- sniper/utils.py
+++ sniper/utils.py
@@ -24,11 +24,15 @@ class _CachedProperty:
         self.__doc__ = getattr(func, '__doc__')
         self.func = func
 
+    @property
+    def name(self):
+        return self.func.__name__
+
     def __get__(self, obj, cls):
         if obj is None:
             return self
 
-        value = obj.__dict__[self.func.__name__] = self.func(obj)
+        value = obj.__dict__[self.name] = self.func(obj)
         return value
 
 

1d75dd75164b932a848efd5d883ec416ffc6ed95
Elephant Liu <lexdene@gmail.com>
2017-07-15 18:50:02 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 with open('README.rst') as f:
     LONG_DESCRIPTION = f.read()
 
 setup(
     name="sniper",
-    version="0.0.4",
+    version="0.0.5",
     description="an asynchronous restful web framework base on asyncio",
     long_description=LONG_DESCRIPTION,
     url="https://github.com/lexdene/py-sniper",
     license='GPLv3',
     author="Elephant Liu",
     author_email="lexdene@gmail.com",
-    packages=['sniper'],
+    packages=find_packages(exclude=['tests/*']),
     platforms=['any'],
     classifiers=[
         'Development Status :: 3 - Alpha',
